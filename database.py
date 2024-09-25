9affce15869633c6442bc53422d4b88e1b5f62f5
Mohit Punglia <110087160+MohitPunglia@users.noreply.github.com>
2024-09-25 23:01:22 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,3 +1,11 @@
+
+def execute_query(query, params=()):
+    conn = sqlite3.connect('finance.db')
+    c = conn.cursor()
+    c.execute(query, params)
+    conn.commit()
+    conn.close()
+
 def fetch_all(query, params=()):
     conn = sqlite3.connect('finance.db')
     c = conn.cursor()
