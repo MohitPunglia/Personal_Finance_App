5630ba3c9cf42584003c5f2d757f1eb394646875
Mohit Punglia <110087160+MohitPunglia@users.noreply.github.com>
2024-09-28 22:15:08 +0100

    Update expenses.py

--- expenses.py
+++ expenses.py
@@ -1,4 +1,8 @@
+from database import execute_query, fetch_all
 
+def add_expense(date, category, amount):
+    query = "INSERT INTO expenses (date, category, amount) VALUES (?, ?, ?)"
+    execute_query(query, (date, category, amount))
 
 def view_expenses():
     query = "SELECT * FROM expenses"
