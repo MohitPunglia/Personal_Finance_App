3e919c9c6cbc4eb52815586d0f72059c5acf7b16
Mohit Punglia <110087160+MohitPunglia@users.noreply.github.com>
2024-10-01 00:02:30 +0100

    Update test_imports.py

--- test_imports.py
+++ test_imports.py
@@ -3,3 +3,9 @@ try:
     print("Imports from expenses.py successful")
 except ImportError as e:
     print(f"Error importing from expenses.py: {e}")
+
+try:
+    from budget import set_budget
+    print("Imports from budget.py successful")
+except ImportError as e:
+    print(f"Error importing from budget.py: {e}")
