db0be9a84544bc55bfa518e2041baf50d14f66e6
Mohit Punglia <110087160+MohitPunglia@users.noreply.github.com>
2024-10-10 22:27:07 +0100

    Update ui.py

--- ui.py
+++ ui.py
@@ -1,3 +1,6 @@
 def set_reminder(reminder_time, message):
     scheduler.enterabs(reminder_time, 1, show_reminder, (message,))
     scheduler.run()
+
+def show_reminder(message):
+    messagebox.showinfo("Reminder", message)
